ca5ebba401efd7479d1cb482c809e8541aed514a
Alex Scarlett <alex.scarlett91@gmail.com>
2019-03-06 16:27:05 -0500

    More design to level materials, added OpenDoor class and function using floor plate and trigger volume

--- Source/BuildingEscape/OpenDoor.cpp
+++ Source/BuildingEscape/OpenDoor.cpp
@@ -19,15 +19,16 @@ UOpenDoor::UOpenDoor()
 void UOpenDoor::BeginPlay()
 {
 	Super::BeginPlay();
+}
 
+void UOpenDoor::OpenDoor()
+{
 	// Find owning Actor
 	AActor* Owner = GetOwner();
 	// Create a rotator
 	FRotator NewRotation = FRotator(0.f, -60.f, 0.f);
 	// Set door rotation
 	Owner->SetActorRotation(NewRotation);
-
-	
 }
 
 
@@ -36,6 +37,11 @@ void UOpenDoor::TickComponent(float DeltaTime, ELevelTick TickType, FActorCompon
 {
 	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
 
-	// ...
+	// Poll the Trigger Volume
+	// If the ActorThatOpens is in the volume
+	if (PressurePlate && PressurePlate->IsOverlappingActor(ActorThatOpens))
+	{
+		OpenDoor();
+	}
 }
 
